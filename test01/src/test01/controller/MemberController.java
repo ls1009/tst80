6a2de68410d078eab21d53df7778cd175529b42b
rlgkr903 <sjwl0903@naver.com>
2016-04-21 18:41:43 +0900

    .

--- test01/src/test01/controller/MemberController.java
+++ test01/src/test01/controller/MemberController.java
@@ -3,43 +3,66 @@ package test01.controller;
 import java.util.List;
 import java.util.Scanner;
 
-import test01.annotation.Controller;
-import test01.annotation.RequestMapping;
 import test01.command.CommandUtil;
 import test01.dao.MemberDao;
 import test01.domain.Member;
 
-@Controller
-@RequestMapping("member/")
 public class MemberController {
   private MemberDao memberDao;
+  Scanner sc = new Scanner(System.in);
 
   public void setMemberDao(MemberDao memberDao) {
     this.memberDao = memberDao;
   }
+  
+	public String prompt() {
+		System.out.print("프로젝트 관리> ");
+		return sc.nextLine().toLowerCase();
+	}
 
-  @RequestMapping("add.do")
-  public void add(Scanner keyScan) {
+	public void service() {
+		String input = null;
+		do {
+			input = prompt();
+			
+			if (input.equals("add")) {
+				add(sc);
+			} else if (input.equals("list")) {
+				list();
+			} else if (input.equals("update")) {
+				update(sc);
+			} else if (input.equals("delete")) {
+				delete(sc);
+			} else if (input.equals("quit")){
+				System.out.println("종료");
+			} else {
+			System.out.println("잘못된 명령어입니다.");
+			}
+		} while (!input.equals("quit"));
+	}
+ 
+
+  public void add(Scanner sc) {
     Member member = new Member();
 
     
     System.out.print("이름? ");
-    member.setName(keyScan.nextLine());
+    member.setName(sc.nextLine());
 
     System.out.print("이메일? ");
-    member.setEmail(keyScan.nextLine());
+    member.setEmail(sc.nextLine());
 
     System.out.print("암호? ");
-    member.setPassword(keyScan.nextLine());
+    member.setPassword(sc.nextLine());
 
     System.out.print("전화? ");
-    member.setTel(keyScan.nextLine());
+    member.setTel(sc.nextLine());
     
     System.out.print("역할? ");
-    member.setPart(keyScan.nextLine());
+    member.setPart(sc.nextLine());
     
 
-    if (CommandUtil.confirm(keyScan, "저장하시겠습니까?")) {
+    if (CommandUtil.confirm(sc, "저장하시겠습니까?")) {
       try {
         memberDao.insert(member);
         System.out.println("저장하였습니다.");
@@ -52,13 +75,13 @@ public class MemberController {
     }
   }
   
-  @RequestMapping("delete.do")
-  public void delete(Scanner keyScan) {
+
+  public void delete(Scanner sc) {
     try {
       System.out.print("삭제할 회원 번호는? ");
-      int no = Integer.parseInt(keyScan.nextLine());
+      int no = Integer.parseInt(sc.nextLine());
   
-      if (CommandUtil.confirm(keyScan, "정말 삭제하시겠습니까?")) {
+      if (CommandUtil.confirm(sc, "정말 삭제하시겠습니까?")) {
         int count = memberDao.delete(no);
         if (count > 0) {
           System.out.println("삭제하였습니다.");
@@ -74,7 +97,7 @@ public class MemberController {
     }
   }
   
-  @RequestMapping("list.do")
+
   public void list() {
     try {
       List<Member> members = memberDao.selectList();
@@ -88,11 +111,11 @@ public class MemberController {
     }
   }
   
-  @RequestMapping("update.do")
-  public void update(Scanner keyScan) {
+
+  public void update(Scanner sc) {
     try {
       System.out.print("변경할 회원 번호는? ");
-      int no = Integer.parseInt(keyScan.nextLine());
+      int no = Integer.parseInt(sc.nextLine());
   
       Member member = memberDao.selectOne(no);
       if (member == null) {
@@ -101,21 +124,21 @@ public class MemberController {
       }
       
       System.out.printf("이름(%s)? ", member.getName());
-      member.setName(keyScan.nextLine());
+      member.setName(sc.nextLine());
   
       System.out.printf("이메일(%s)? ", member.getEmail());
-      member.setEmail(keyScan.nextLine());
+      member.setEmail(sc.nextLine());
   
       System.out.printf("암호(%s)? ", member.getPassword());
-      member.setPassword(keyScan.nextLine());
+      member.setPassword(sc.nextLine());
   
       System.out.printf("전화(%s)? ", member.getTel());
-      member.setTel(keyScan.nextLine());
+      member.setTel(sc.nextLine());
       
       System.out.printf("역할(%s)? ", member.getPart());
-      member.setPart(keyScan.nextLine());
+      member.setPart(sc.nextLine());
   
-      if (CommandUtil.confirm(keyScan, "변경하시겠습니까?")) {
+      if (CommandUtil.confirm(sc, "변경하시겠습니까?")) {
         int count = memberDao.update(member);
         if (count > 0) {
           System.out.println("변경하였습니다.");
@@ -130,13 +153,5 @@ public class MemberController {
     }
   }
 }
-
-
-
-
-
-
-
-
 
 
