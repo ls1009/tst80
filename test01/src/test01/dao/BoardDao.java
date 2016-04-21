aa50eb0cffb9b6b89a2dfff3a78d87a1d61f9738
rlgkr903 <sjwl0903@naver.com>
2016-04-21 12:50:58 +0900

    .

--- test01/src/test01/dao/BoardDao.java
+++ test01/src/test01/dao/BoardDao.java
@@ -1,5 +1,99 @@
 package test01.dao;
 
-public class BoardDao {
+import java.util.HashMap;
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import test01.domain.Board;
+
+public class BoardDao {
+	SqlSessionFactory sqlSessionFactory;
+	public BoardDao() {
+	}
+	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+		this.sqlSessionFactory = sqlSessionFactory;
+	}
+
+	public List<Board> selectList() throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		try {
+			return sqlSession.selectList("BoardDao.selectList");
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	public Board selectOne(int no) throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		try {
+			HashMap<String, Object> paramMap = new HashMap<>();
+			paramMap.put("no", no);
+
+			return sqlSession.selectOne("BoardDao.selectOne", paramMap);
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	public Board selectOneByEmail(String email) {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		try {
+			HashMap<String, Object> paramMap = new HashMap<>();
+			paramMap.put("email", email);
+
+			return sqlSession.selectOne("BoardDao.selectOne", paramMap);
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	public int insert(Board Board) throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		try {
+			return sqlSession.insert("BoardDao.insert", Board);
+
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	public int update(Board Board) throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		try {
+			int count = sqlSession.update("BoardDao.update", Board);
+			sqlSession.commit();
+			return count;
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	public int delete(int no) throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		try {
+			int count = sqlSession.delete("BoardDao.delete", no);
+			sqlSession.commit();
+			return count;
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	public boolean isBoard(String email, String password) {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		try {
+			HashMap<String, String> paramMap = new HashMap<>();
+			paramMap.put("email", email);
+			paramMap.put("password", password);
+			int count = sqlSession.selectOne("BoardDao.isBoard", paramMap);
+			if (count > 0)
+				return true;
+			else
+				return false;
+		} finally {
+			sqlSession.close();
+		}
+	}
 }
