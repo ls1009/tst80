e57d99ce5eff9752ddf455e2bd815f411b5b6af9
rlgkr903 <sjwl0903@naver.com>
2016-04-21 12:50:39 +0900

    .

--- test01/src/test01/controller/BoardController.java
+++ test01/src/test01/controller/BoardController.java
@@ -1,5 +1,103 @@
 package test01.controller;
 
-public class BoardController {
+import java.sql.Date;
+import java.util.List;
+import java.util.Scanner;
 
+import test01.command.CommandUtil;
+import test01.dao.BoardDao;
+import test01.domain.Board;
+
+public class BoardController {
+	private BoardDao boardDao;
+
+	public void setBoardDao(BoardDao boardDao) {
+		this.boardDao = boardDao;
+	}
+
+	public void doAdd(Scanner sc) {
+		Board board = new Board();
+
+		System.out.print("제목?");
+		board.setTitle(sc.nextLine());
+		System.out.print("내용?");
+		board.setContent(sc.nextLine());
+		System.out.println("암호?");
+		board.setPassword(sc.nextLine());
+		board.setCreateDate(new Date(System.currentTimeMillis()));
+		if (CommandUtil.confirm(sc, "저장하시겠습니까?")) {
+			try {
+				boardDao.insert(board);
+				System.out.println("저장하였습니다.");
+			} catch (Exception e) {
+				System.out.println("데이터를 저장할 수 없습니다.");
+			}
+		} else {
+			System.out.println("저장을 취소하였습니다.");
+		}
+	}
+
+	public void doList() {
+		try {
+			List<Board> boards = boardDao.selectList();
+
+			for (Board board : boards) {
+				System.out.printf("%d, %s, %s, %d, %s\n", board.getNo(), board.getTitle(), board.getContent(),
+						board.getViews(), board.getWriter());
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("게시물 데이터 로딩 실패!", e);
+		}
+	}
+
+	public void doUpdate(Scanner sc) {
+		try {
+			System.out.print("변경할 게시물 번호?");
+			int no = Integer.parseInt(sc.nextLine());
+
+			Board board = boardDao.selectOne(no);
+			if (board == null) {
+				System.out.println("유효하지 않은 번호입니다.");
+				return;
+			}
+			System.out.printf("제목(%s)?", board.getTitle());
+			board.setTitle(sc.nextLine());
+			System.out.printf("내용(%s)?", board.getContent());
+			board.setContent(sc.nextLine());
+			board.setEditDate(new Date(System.currentTimeMillis()));
+
+			if (CommandUtil.confirm(sc, "변경하시겠습니까?")) {
+				int count = boardDao.update(board);
+				if (count > 0) {
+					System.out.println("변경하였습니다.");
+				} else {
+					System.out.println("유효하지 않은 번호이거나, 삭제된 번호입니다.");
+				}
+			} else {
+				System.out.println("변경을 취소하였습니다.");
+			}
+		} catch (Exception e) {
+			System.out.println("데이터 처리에 실패했습니다.");
+		}
+	}
+
+	public void doDelete(Scanner sc) {
+		try {
+			System.out.print("삭제할 게시물 번호?");
+			int no = Integer.parseInt(sc.nextLine());
+
+			if (CommandUtil.confirm(sc, "삭제하시겠습니까?")) {
+				int count = boardDao.delete(no);
+				if (count > 0) {
+					System.out.println("삭제하였습니다.");
+				} else {
+					System.out.println("유효하지 않은 번호이거나, 삭제된 번호입니다.");
+				}
+			} else {
+				System.out.println("삭제를 취소하였습니다.");
+			}
+		} catch (Exception e) {
+			System.out.println("데이터 처리에 실패했습니다.");
+		}
+	}
 }
