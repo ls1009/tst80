b2606c80b84ee7d23bbe1f9e30f874c3a3989d10
ls1009 <ls_10@naver.com>
2016-04-21 11:40:37 +0900

    .

---
+++ test01/src/command/CommandUtil.java
@@ -0,0 +1,21 @@
+package command;
+
+import java.util.Scanner;
+
+public class CommandUtil {
+  static public boolean confirm(Scanner sc, String m) {
+    String input = null;
+    while(true) {
+      System.out.print(m);
+      input = sc.nextLine().toLowerCase();
+      
+      if(input.equals("y")) {
+        return true;
+      } else if(input.equals("n")) {
+        return false;
+      } else {
+        System.out.println("다시 입력 하십시오.");
+      }
+    }
+  }
+}
